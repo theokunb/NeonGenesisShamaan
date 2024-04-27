c09929e45c56814a45a331098e247d216416778a
Алексей Макаров <makarovalexeyvery@gmail.com>
2024-04-28 06:50:53 +0900

    added cinematic and menu

--- Assets/Scripts/UI/Cinematic1View.cs
+++ Assets/Scripts/UI/Cinematic1View.cs
@@ -1,28 +1,49 @@
 using DG.Tweening;
 using DG.Tweening.Plugins.Options;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Cinematic1View : BaseView
 {
     [SerializeField] private Image _scene1;
-
     [SerializeField] private CanvasGroup _canvasGroup;
     [SerializeField] private Animator _animator;
 
+    private Scene1AnimationHandler _handler;
+
+    public event Action Ended;
+
+    private void Awake()
+    {
+        _handler = GetComponentInChildren<Scene1AnimationHandler>();
+    }
+
     public override void Hide()
     {
-        gameObject.SetActive(false);
+        _handler.Handle -= OnHandle;
+        _canvasGroup.DOFade(0, 2).OnComplete(() =>
+        {
+            gameObject.SetActive(false);
+        });
     }
 
     public override void Show()
     {
         gameObject.SetActive(true);
+
+        _handler.Handle += OnHandle;
     }
+
+    private void OnHandle()
+    {
+        Ended?.Invoke();
+    }
+
     private void Start()
     {
         _canvasGroup.alpha = 0;
-        _canvasGroup.DOFade(0.2f, 5)
+        _canvasGroup.DOFade(0.3f, 5)
             .OnComplete(() =>
             {
                 _canvasGroup.DOFade(1, 2)
--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,8 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    private const string IsCinematicWatched = "watched";
 
+    [SerializeField] private MenuView _menuView;
+    [SerializeField] private Cinematic1View _chematic1View;
+    [SerializeField] private Cinematic2View _chematic2View;
+
+    private void Awake()
+    {
+        var cinematicWatched = PlayerPrefs.GetInt(IsCinematicWatched, 0);
+
+        if (cinematicWatched == 0)
+        {
+            _chematic1View.Show();
+
+            _chematic1View.Ended += OnCinematic1Ended;
+        }
+        else
+        {
+            _menuView.Show();
+        }
+    }
+
+    private void OnCinematic1Ended()
+    {
+        _chematic1View.Hide();
+        _chematic1View.Ended -= OnCinematic1Ended;
+
+        _chematic2View.Show();
+        _chematic2View.Ended += OnCinematic2Ended;
+    }
+
+    private void OnCinematic2Ended()
+    {
+        _chematic2View.Hide();
+        _chematic2View.Ended -= OnCinematic2Ended;
+
+        PlayerPrefs.SetInt(IsCinematicWatched, 1);
+
+        _menuView.Show();
+    }
 }
