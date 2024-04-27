632f927699843990545c94ece998a1281ec70a33
DimaHovrov <DimaHovrov@example.com>
2024-04-27 12:24:06 +0900

    Add Damagable

---
+++ Assets/Scripts/Common/Damagable.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damageble : MonoBehaviour
+{
+    [SerializeField]
+    float hp = 100;
+
+    public float HitPoint { get => hp; }
+
+    public event Action<object> OnCharacterDeadEvent;
+    public event Action<object> OnCharacterTakeDamageEvent;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void Damage(float damage)
+    {
+        hp -= damage;
+
+        OnCharacterTakeDamageEvent?.Invoke(this);
+
+
+        if (hp <= 0)
+        {
+            OnCharacterDeadEvent?.Invoke(this);
+        }
+    }
+}
