36023eedb6848a665fb2cb273aa7254d2555a8a8
Алексей Макаров <makarovalexeyvery@gmail.com>
2024-04-27 20:44:09 +0900

    added lose view

---
+++ Assets/Scripts/UI/BaseView.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public abstract class BaseView : MonoBehaviour
+{
+    public abstract void Show();
+    public abstract void Hide();
+}
--- Assets/Scripts/UI/LoseView.cs
+++ Assets/Scripts/UI/LoseView.cs
@@ -1,18 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class LoseView : MonoBehaviour
+public class LoseView : BaseView, IService
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _backgroundAlpha;
+    [SerializeField] private float _fadeTime;
+
+    private CanvasGroup _canvasGroup;
+
+    private void Awake()
     {
-        
+        _canvasGroup = GetComponentInChildren<CanvasGroup>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Hide()
     {
-        
+        gameObject.SetActive(false);
+
+        Time.timeScale = 1;
+    }
+
+    public override void Show()
+    {
+        gameObject.SetActive(true);
+        _canvasGroup.alpha = 0;
+        Time.timeScale = 0;
+
+        _canvasGroup.DOFade(_backgroundAlpha, _fadeTime).SetUpdate(true);
+    }
+
+    public void OnReplay()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnExit()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
     }
 }
