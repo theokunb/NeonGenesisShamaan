77e4f54ce836adfa0c3618a9e56f3bf3b36b0cec
osipov_de2 <djspektr2001@mail.ru>
2024-04-28 08:35:18 +0900

    commit

---
+++ Assets/Scripts/UI/RandomDetails.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomDetails : MonoBehaviour
+{
+    public GameObject[] details;
+    public List<Transform> spawnPoints;
+    void Start()
+    {
+        spawnPoints = new List<Transform>(spawnPoints);
+        SpawnDetails();
+    }
+    void SpawnDetails()
+    {
+
+        var rand = Random.Range(details.Length, 6);
+        for(int i = 0; i < 10; i++)
+        {
+            var spawn = Random.Range(0, spawnPoints.Count);
+            Instantiate(details[Random.Range(0,3)], spawnPoints[spawn].transform.position, Quaternion.identity);
+            spawnPoints.RemoveAt(spawn);
+        }
+    }
+}
