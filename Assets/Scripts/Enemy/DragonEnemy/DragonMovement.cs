576ea2d582330f751466815b3fbd60da69f818b3
DimaHovrov <DimaHovrov@example.com>
2024-04-27 19:24:59 +0900

    Add dragon enemy

---
+++ Assets/Scripts/Enemy/DragonEnemy/DragonMovement.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonMovement : MonoBehaviour
+{
+    enum DragonStates
+    {
+        SelectSpawn,
+        WaitState,
+        ShowState,
+        ShootState,
+        HideState
+    }
+
+    [SerializeField]
+    public List<Transform> spawnDragons;
+
+    Transform currentSpawn;
+
+    [SerializeField]
+    float downDistance;
+    GameObject player;
+
+    DragonStates state;
+
+    [SerializeField]
+    float speed, waitTime, startDistance;
+
+    ShootDragon shoot;
+
+    //Vector3 startPos;
+
+    float time;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        shoot = GetComponent<ShootDragon>();
+
+        state = DragonStates.SelectSpawn;
+
+        //startPos = transform.position;
+    }
+
+
+    void Update()
+    {
+        float distanceBetween = Vector3.Distance(transform.position, player.transform.position);
+
+
+        switch (state)
+        {
+            case DragonStates.SelectSpawn:
+                SelectSpawn();
+                break;
+            case DragonStates.WaitState:
+                WaitStateNext(distanceBetween);
+                break;
+            case DragonStates.ShowState:
+                ShowMove();
+                break;
+            case DragonStates.ShootState:
+                Shoot();
+                break;
+            case DragonStates.HideState:
+                HideMove();
+                break;
+        }
+    }
+
+    void SelectSpawn()
+    {
+        var len = spawnDragons.Count;
+        var randomIndex = Random.Range(0, len);
+
+        currentSpawn = spawnDragons[randomIndex];
+
+        state = DragonStates.WaitState;
+        transform.position = new Vector2(currentSpawn.position.x, currentSpawn.position.y - downDistance);
+    }
+
+    void WaitStateNext(float distanceBetween)
+    {
+        time += Time.deltaTime;
+        if (distanceBetween <= startDistance && time >= waitTime)
+        {
+            state = DragonStates.ShowState;
+            time = 0;
+        }
+    }
+
+    float distance = 0;
+    private void ShowMove()
+    {
+        transform.Translate(0,speed * Time.deltaTime, 0);
+        distance += speed * Time.deltaTime;
+
+        if (distance >= downDistance)
+        {
+            distance = 0;
+            state = DragonStates.ShootState;
+        }
+    }
+
+
+    private void HideMove()
+    {
+        transform.Translate(0, -speed * Time.deltaTime, 0);
+        distance += speed * Time.deltaTime;
+
+        if (distance >= downDistance + 1)
+        {
+            distance = 0;
+            state = DragonStates.SelectSpawn;
+        }
+    }
+    void Shoot()
+    {
+        bool isShoot = shoot.Shoot();
+
+        if (isShoot == false)
+        {
+            time += Time.deltaTime;
+
+            if (time >= waitTime)
+            {
+                state = DragonStates.HideState;
+                time = 0;
+                shoot.EndFire();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/DragonEnemy/ShootDragon.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootDragon : MonoBehaviour
+{
+    [SerializeField]
+    GameObject bullet;
+
+    Transform playerPoint;
+
+    [SerializeField]
+    float delay, maxCountFire;
+
+    float time = 0;
+
+    float currentCount = 0;
+    void Start()
+    {
+        playerPoint = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+
+    public bool Shoot()
+    {
+        time += Time.deltaTime;
+
+        if (time >= delay && currentCount <= maxCountFire)
+        {
+            Fire();
+            currentCount++;
+            time = 0;
+        }
+
+        if (currentCount >= maxCountFire)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    void Fire()
+    {
+        GameObject currentBullet = Instantiate(bullet, transform.position, Quaternion.identity);
+
+        Bullet bulletComponent = currentBullet.GetComponent<Bullet>();
+
+        var direction = GetBulletDirection();
+
+        bulletComponent.SetDirection(direction);
+    }
+
+    public float GetBulletDirection()
+    {
+        var playerPosition = playerPoint.position;
+
+        var directionVector = new Vector2(playerPosition.x, playerPosition.y) - new Vector2(transform.position.x, transform.position.y);
+
+        var playerDirection = playerPosition.y - transform.position.y;
+
+        float angleToEuler = Vector2.Angle(transform.right, directionVector);
+
+        if (playerDirection < 0)
+        {
+            angleToEuler = 360 - angleToEuler;
+        }
+
+        return angleToEuler;
+    }
+
+    public void EndFire()
+    {
+        currentCount = 0;
+        time = 0;
+    }
+}
